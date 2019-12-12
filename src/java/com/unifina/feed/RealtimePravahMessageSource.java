45f7712a905a03dc8207eb62bd0dfa597b37ec4a
Abhishek Upperwal <a.upperwal@gmail.com>
2019-12-12 22:51:07 +0530

    unsub on close

--- src/java/com/unifina/feed/RealtimePravahMessageSource.java
+++ src/java/com/unifina/feed/RealtimePravahMessageSource.java
@@ -3,6 +3,7 @@ package com.unifina.feed;
 import com.streamr.client.exceptions.EncryptedContentNotParsableException;
 import com.streamr.client.protocol.message_layer.MessageRef;
 import com.streamr.client.protocol.message_layer.StreamMessage;
+import com.streamr.client.protocol.message_layer.StreamMessageV28;
 import com.streamr.client.utils.StreamPartition;
 import com.unifina.utils.Globals;
 import in.soket.Data;
@@ -10,24 +11,29 @@ import in.soket.StreamCatcher;
 import org.apache.log4j.Logger;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.util.*;
 
 public class RealtimePravahMessageSource extends StreamPravahMessageSource {
 
 	private static final Logger log = Logger.getLogger(RealtimePravahMessageSource.class);
 
+	private List geo;
+	private final String channel = "/AirQuality";
+
+
 	public RealtimePravahMessageSource(Globals globals, StreamMessageConsumer consumer, Collection<StreamPartition> streamPartitions) {
 		super(globals, consumer, streamPartitions);
 
-		List geo = new ArrayList<String>();
+		geo = new ArrayList<String>();
 		geo.add("/in/ncr");
 
-		pravahRPC.subscribe("/AirQuality", geo, new StreamCatcherLogic(consumer));
+		pravahRPC.subscribe(channel, geo, new StreamCatcherLogic(consumer));
 	}
 
 	@Override
 	public void close() {
-
+		pravahRPC.unsubscribe(channel, geo);
 	}
 
 	private static class StreamCatcherLogic implements StreamCatcher {
@@ -42,72 +48,33 @@ public class RealtimePravahMessageSource extends StreamPravahMessageSource {
 		public void onNext(Data data) {
 			Map m = new LinkedHashMap();
 			m.put("topic", data.getTopic());
+			try {
+				m.put("raw", data.getRaw().toString("utf-8"));
+			} catch (UnsupportedEncodingException e) {
+				e.printStackTrace();
+			}
+
+			// Hack
+			m.put("start", "");
+			m.put("groupKey", "");
 
 			log.debug("StreamCatcherLogic: Got message");
 			log.debug("Message got: " + data.getTopic());
 
-			this.streamMessageConsumer.accept(new StreamMessage(
-					111,
-					StreamMessage.ContentType.GROUP_KEY_RESPONSE_SIMPLE,
-					StreamMessage.EncryptionType.NONE,
+			StreamMessageV28 msg = new StreamMessageV28(
+					data.getTopic(),
+					0,
+					System.currentTimeMillis(),
+					100000,
+					0L,
+					0L,
+					StreamMessage.ContentType.GROUP_KEY_RESET_SIMPLE,
 					m
-			) {
+			);
 
-				@Override
-				public String getStreamId() {
-					return "";
-				}
+			log.debug("After Msg formed");
 
-				@Override
-				public int getStreamPartition() {
-					return 0;
-				}
-
-				@Override
-				public long getTimestamp() {
-					return 0;
-				}
-
-				@Override
-				public long getSequenceNumber() {
-					return 0;
-				}
-
-				@Override
-				public String getPublisherId() {
-					return null;
-				}
-
-				@Override
-				public String getMsgChainId() {
-					return null;
-				}
-
-				@Override
-				public MessageRef getPreviousMessageRef() {
-					return null;
-				}
-
-				@Override
-				public SignatureType getSignatureType() {
-					return null;
-				}
-
-				@Override
-				public String getSignature() {
-					return null;
-				}
-
-				@Override
-				public void setSignatureType(SignatureType signatureType) {
-
-				}
-
-				@Override
-				public void setSignature(String s) {
-
-				}
-			});
+			this.streamMessageConsumer.accept(msg);
 		}
 
 		@Override
