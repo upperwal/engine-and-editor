23d7b8b0fe7bb4f41e3431b3b4af79e7de2921f3
Abhishek Upperwal <a.upperwal@gmail.com>
2019-12-13 23:58:28 +0530

    added pravah stream to db

--- grails-app/migrations/core/2019-12-13-pravah-source-module.groovy
+++ grails-app/migrations/core/2019-12-13-pravah-source-module.groovy
@@ -1,8 +1,8 @@
 package core
 databaseChangeLog = {
-	changeSet(author: "eric", id: "add-stream-modules-4") {
+	changeSet(author: "upperwal", id: "add-stream-modules-4") {
 		insert(tableName: "module") {
-			column(name: "id", valueNumeric: 529)
+			column(name: "id", valueNumeric: 50001)
 			column(name: "version", valueNumeric: 0)
 			column(name: "category_id", valueNumeric: 53)
 			column(name: "implementing_class", value: "com.unifina.signalpath.pravah.PravahStreamModule")
@@ -11,6 +11,18 @@ databaseChangeLog = {
 			column(name: "type", value: "module")
 			column(name: "module_package_id", valueNumeric: 1)
 			column(name: "json_help", value: '{"params":{"channel":"the channel from which data is subscribed"},"paramNames":["channel"],"outputs":{"topic":"name of the topic","raw":"raw data stream"},"outputNames":["topic","raw"],"helpText":"<p>Create a new stream.</p>"}')
+		}
+	}
+
+	changeSet(author: "upperwal", id: "tour-resources-6") {
+
+		// Grant public read permission to demo streams
+		insert(tableName: "permission") {
+			column(name: "version", valueNumeric: 0)
+			column(name: "clazz", value: "com.unifina.domain.data.Stream")
+			column(name: "operation", value: "read")
+			column(name: "string_id", value: "/AirQuality/in/ncr")
+			column(name: "anonymous", valueBoolean: true)
 		}
 	}
 }
