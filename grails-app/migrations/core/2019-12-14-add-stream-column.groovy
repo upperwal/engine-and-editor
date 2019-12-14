4747de74f0317a33e930489301ba11ae0a4c9834
Abhishek Upperwal <a.upperwal@gmail.com>
2019-12-14 14:20:26 +0530

    removed channel const

--- grails-app/migrations/core/2019-12-14-add-stream-column.groovy
+++ grails-app/migrations/core/2019-12-14-add-stream-column.groovy
@@ -3,9 +3,7 @@ package core
 databaseChangeLog = {
 	changeSet(author: "upperwal", id: "update-table") {
 		addColumn(tableName: "stream") {
-			column(name: "channel", type: "varchar(1024)") {
-				constraints(nullable: "false")
-			}
+			column(name: "channel", type: "varchar(1024)")
 		}
 	}
 }
