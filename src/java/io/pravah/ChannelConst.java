57246c893155c879bd58f7996c302b01d981eb29
Abhishek Upperwal <a.upperwal@gmail.com>
2019-12-15 11:36:58 +0530

    added GTFS proto support

--- src/java/io/pravah/ChannelConst.java
+++ src/java/io/pravah/ChannelConst.java
@@ -2,4 +2,5 @@ package io.pravah;
 
 public class ChannelConst {
 	public static final String AIR_QUALITY = "/AirQuality";
+	public static final String PUBLIC_BUS = "/PublicBus";
 }
--- src/java/io/pravah/Decoder.java
+++ src/java/io/pravah/Decoder.java
@@ -4,12 +4,15 @@ import com.google.protobuf.ByteString;
 import com.google.protobuf.InvalidProtocolBufferException;
 import com.google.protobuf.Message;
 import io.pravah.air.quality.AirQuality;
+import io.pravah.transit.GtfsRealtime;
 
 public class Decoder {
 	static public Message decode(String channel, ByteString data) throws InvalidProtocolBufferException {
 		switch(channel) {
 			case ChannelConst.AIR_QUALITY:
 				return AirQuality.FeedMessage.parseFrom(data);
+			case ChannelConst.PUBLIC_BUS:
+				return GtfsRealtime.FeedMessage.parseFrom(data);
 			default:
 				return null;
 		}
